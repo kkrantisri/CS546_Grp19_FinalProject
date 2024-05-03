e3dc3ac445b285c6a6f71a87110a7dcc28ab899c
Dtharuni <144611337+Dtharuni@users.noreply.github.com>
2024-05-03 18:26:44 -0400

    Update index.js

---
+++ data/index.js
@@ -0,0 +1,6 @@
+import * as userDataFunctions from './users.js';
+import * as sessionDataFunctions from './sessions.js';
+import * as postDataFunctions from './posts.js';
+export const userData = userDataFunctions;
+export const sessionData = sessionDataFunctions;
+export const postData = postDataFunctions;
