8f2a1465f69be2bd6350df17baaa64830ef1cde8
kkrantisri <kkalepal@stevens.edu>
2024-05-04 16:49:56 -0400

    minor

--- routes/auth.js
+++ routes/auth.js
@@ -1,39 +1,62 @@
-import { Router } from 'express';
-import { users } from '../config/mongoCollections.js';
-import { addUser, loginUser } from '../data/users.js';
-import  { checkId, checkString, checkStringArray, checkEmail, checkRating, isValidDate, isTimeSlotValid } from '../helper.js';
-import bcrypt from 'bcrypt';
+import { Router } from "express";
+import { users } from "../config/mongoCollections.js";
+import { addUser, loginUser } from "../data/users.js";
+import {
+  checkId,
+  checkString,
+  checkStringArray,
+  checkEmail,
+  checkRating,
+  isValidDate,
+  isTimeSlotValid,
+} from "../helper.js";
+import bcrypt from "bcrypt";
 const router = Router();
 
-router.route('/')
-  .get(async (req, res) => {
-    try {
-      return res.json({ error: 'YOU SHOULD NOT BE HERE!' });
-    } catch (error) {
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  });
+router.route("/").get(async (req, res) => {
+  try {
+    return res.json({ error: "YOU SHOULD NOT BE HERE!" });
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 
-  router.route('/register')
+router
+  .route("/register")
   .get(async (req, res) => {
     try {
-      res.status(200).render('signup', { title: 'Signup Page' });
+      res.status(200).render("signup", { title: "Signup Page" });
     } catch (error) {
-      res.status(500).json({ error: 'Internal Server Error' });
+      res.status(500).json({ error: "Internal Server Error" });
     }
-  }).post(async (req, res) => {
+  })
+  .post(async (req, res) => {
     try {
-      let { username, password, email, fullname, major, languages, coursesEnrolled, reviews, bio, gradyear } = req.body;
-      const validatedUsername =  checkString(username, 'username');
-      const validatedPassword =  checkString(password, 'password');
-      const validatedEmail =  checkEmail(email);
-      const validatedFullName =  checkString(fullName, 'fullName');
-      const validatedMajor =  checkString(major, 'major');
-      const validatedLanguages =  checkStringArray(languages, 'languages');
-      const validatedCoursesEnrolled =  checkStringArray(coursesEnrolled, 'coursesEnrolled');
-      const validatedBio =  checkString(bio, 'bio');
-      const validatedGradYear =  checkPositiveNumber(gradYear, 'gradYear');
-      
+      let {
+        username,
+        password,
+        email,
+        fullname,
+        major,
+        languages,
+        coursesEnrolled,
+        reviews,
+        bio,
+        gradyear,
+      } = req.body;
+      const validatedUsername = checkString(username, "username");
+      const validatedPassword = checkString(password, "password");
+      const validatedEmail = checkEmail(email);
+      const validatedFullName = checkString(fullName, "fullName");
+      const validatedMajor = checkString(major, "major");
+      const validatedLanguages = checkStringArray(languages, "languages");
+      const validatedCoursesEnrolled = checkStringArray(
+        coursesEnrolled,
+        "coursesEnrolled"
+      );
+      const validatedBio = checkString(bio, "bio");
+      const validatedGradYear = checkPositiveNumber(gradYear, "gradYear");
+
       const newUser = await addUser({
         username: validatedUsername,
         password: validatedPassword,
@@ -43,26 +66,34 @@ router.route('/')
         languages: validatedLanguages,
         coursesEnrolled: validatedCoursesEnrolled,
         bio: validatedBio,
-        gradYear: validatedGradYear
+        gradYear: validatedGradYear,
       });
 
-      res.status(200).render('signup', { message: 'Successfully Registered. You can login now.', title: 'Login' });
+      res.status(200).render("signup", {
+        message: "Successfully Registered. You can login now.",
+        title: "Login",
+      });
     } catch (error) {
-      res.status(400).render('signup', { message: error.message || 'Error: Internal Server Error', title: 'Signup Form' });
+      res.status(400).render("signup", {
+        message: error.message || "Error: Internal Server Error",
+        title: "Signup Form",
+      });
     }
   });
-  router.route('/login')
+router
+  .route("/login")
   .get(async (req, res) => {
     try {
-      res.status(200).render('login', { title: 'Login Form' });
+      res.status(200).render("login", { title: "Login Form" });
     } catch (error) {
-      res.status(500).json({ error: 'Internal Server Error' });
+      res.status(500).json({ error: "Internal Server Error" });
     }
-  }) .post(async (req, res) => {
+  })
+  .post(async (req, res) => {
     try {
       let { username, password } = req.body;
-      username =  checkString(username, 'username');
-      password =  checkString(password, 'password');
+      username = checkString(username, "username");
+      password = checkString(password, "password");
 
       // Authenticate user
       const user = await loginUser(username, password);
@@ -79,34 +110,29 @@ router.route('/')
           coursesEnrolled: user.coursesEnrolled,
           bio: user.bio,
           gradYear: user.gradYear,
-          reviews: user.reviews
+          reviews: user.reviews,
         };
-        res.redirect('/posts');
+        res.redirect("/posts");
       } else {
-        res.status(401).render('login', { message: 'Invalid username or password.', title: 'Login Form' });
+        res.status(401).render("login", {
+          message: "Invalid username or password.",
+          title: "Login Form",
+        });
       }
     } catch (error) {
-      res.status(400).render('login', { message: error.message || 'Error: Internal Server Error', title: 'Login Form' });
+      res.status(400).render("login", {
+        message: error.message || "Error: Internal Server Error",
+        title: "Login Form",
+      });
     }
   });
-router.route('/logout')
-  .get(async (req, res) => {
-    try {
-      req.session.destroy();
-      res.status(200).render('logout');
-    } catch (error) {
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  });
+router.route("/logout").get(async (req, res) => {
+  try {
+    req.session.destroy();
+    res.status(200).render("logout");
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 
-
-  export default router;
-
-
-
-
-
-
-
-
-
+export default router;
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,10 @@
 import {Router} from 'express';
 const router = Router();
-import { getUserById } from '../data/users.js';
+import { addUser, getUserById, getUserByUsername, getUserByEmail, getAllUsers, updateUser, getCoursesbyUserName, getReviewsForUser, addReviewToUser } from '../data/users.js';
+import {postData, userData} from '../data/index.js';
+import  { checkId, checkString, checkStringArray, checkEmail, checkRating, isValidDate, isTimeSlotValid } from '../helper.js';
+
+
 router.route('/users/:id')
   .get(async (req, res) => {
     try {
@@ -15,4 +19,40 @@ router.route('/users/:id')
       res.status(500).json({ error: 'Internal Server Error' });
     }
   });
-  export default router;
+
+// get - myProfile
+router.route('/users/myProfile')
+  .get(async (req, res) => {
+    if (req.session.user) {
+      try {
+        const userInfo = await getUserById(req.session.user);
+        return res.render("users/index", {
+          userInfo: userInfo,
+          userLoggedIn: true
+        }) 
+      } catch (e) {
+          res.status(500).render("error", { errors: e, userLoggedIn: true });
+    }
+  } else {
+    return res.redirect("/login");
+  }
+});
+
+// Get - Edit myProfile
+router.route("/users/myProfileEdit")
+  .get(async (req, res) => {
+  if (req.session.user) {
+    const userInfo = await users.getUserById(req.session.user);
+    return res.render("users/editUser", {
+      userInfo: userInfo,
+      userLoggedIn: true,
+    });
+  } else {
+    return res.redirect("/login");
+  }
+});
+
+// Post - Edit Profile
+
+
+export default router;
